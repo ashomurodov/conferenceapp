b28067e3c684e3ac5b1897ca13bc8e54f34865a3
Ayyub <ayyuubshomurodov@gmail.com>
2023-05-03 02:40:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("ul li a");
+if (window.innerWidth > 1000) {
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href").includes(current)) {
+        link.classList.add("active");
+      }
+    });
+  });
+}
