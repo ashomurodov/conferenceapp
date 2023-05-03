6bcd1a5d34b2c4a08846d8be42b0681bc191a940
Ayyub <ayyuubshomurodov@gmail.com>
2023-05-04 00:28:28 +0500

    add some sections

--- script.js
+++ script.js
@@ -16,8 +16,36 @@ if (window.innerWidth > 1000) {
       link.classList.remove("active");
       if (link.getAttribute("href").includes(current)) {
         link.classList.add("active");
-        console.log(link)
+        console.log(link);
       }
     });
   });
 }
+
+// carusel;
+
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const images = ["carusel0.jpg", "carusel1.jpg", "carusel2.jpg"];
+
+let counter = 0;
+
+next.addEventListener("click", () => {
+  counter++;
+  if (counter >= images.length) {
+    counter = 0;
+  }
+
+  document.querySelector(".caruselImg").src = `./images/${images[counter]}`;
+});
+
+prev.addEventListener("click", () => {
+  counter--;
+  if (counter < 0) {
+    counter = images.length - 1;
+  }
+  console.log(document.querySelector(".caruselImg").src);
+  document.querySelector(".caruselImg").src = `./images/${images[counter]}`;
+  console.log(images[counter]);
+  console.log("hello");
+});
